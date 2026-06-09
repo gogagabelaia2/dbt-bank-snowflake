b84d548ba013516f83a4255bedfbbee57f25b6cb
gogagabelaia2 <goga.gabelaia2@gmail.com>
2026-06-09 12:16:03 +0400

    feat: add customer mart - int_customer_summary and fct_customer_summary

---
+++ models/intermediate/int_customer_summary.sql
@@ -0,0 +1,58 @@
+with customers as(
+    select
+        customer_id,
+        first_name ||' '|| last_name as full_name
+    from {{ref('stg_customers')}}
+),
+loans as(
+    select
+        loan_id,
+        customer_id,
+        outstanding_balance,
+        original_amount,
+        days_past_due
+    from {{ref('stg_loans')}}
+),
+deposits as(
+    select
+        deposit_id,
+        customer_id,
+        principal_amount
+    from {{ref('stg_deposits')}}
+),
+loans_agg as (
+    select
+        customer_id,
+        count(loan_id)                    as total_loans,
+        sum(outstanding_balance)          as total_outstanding,
+        max(case when days_past_due > 90 
+            then 1 else 0 end)            as has_npl,
+        sum(case when days_past_due > 90 
+            then outstanding_balance 
+            else 0 end)                   as npl_balance
+    from loans
+    group by customer_id
+),
+deposits_agg as (
+    select
+        customer_id,
+        sum(principal_amount) as total_deposits
+    from deposits
+    group by customer_id
+),
+final as(
+    select 
+        l.customer_id,
+        c.full_name,
+        l.total_loans,
+        l.total_outstanding,
+        l.has_npl,
+        l.npl_balance,
+        coalesce(d.total_deposits,0) as total_deposits,
+        (l.total_outstanding - coalesce(d.total_deposits,0)) as net_exposure
+    from loans_agg l 
+    left join deposits_agg d on l.customer_id=d.customer_id   
+    left join customers c on l.customer_id=c.customer_id 
+    
+)
+select * from final
