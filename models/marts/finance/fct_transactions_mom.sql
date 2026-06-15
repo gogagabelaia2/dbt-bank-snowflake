f0492cbc84a1a48730bc23b2665474063944bafe
gogagabelaia2 <goga.gabelaia2@gmail.com>
2026-06-15 13:41:40 +0400

    feat: add fct_transactions_mom mart model and schema.yml

---
+++ models/marts/finance/fct_transactions_mom.sql
@@ -0,0 +1,16 @@
+with monthly_volume as(
+    select 
+        month,
+        total_volume,
+        lag(total_volume) over (order by month ) as prev_month_volume
+    from {{ref('int_transactions_monthly')}}
+),
+growth_pct as(
+    select
+        month,
+        total_volume,
+        prev_month_volume,
+        round((total_volume - prev_month_volume)/nullif(prev_month_volume,0) * 100 , 2) as volume_growth_pct
+    from monthly_volume 
+)
+select * from growth_pct
