3aa1197a157430f62b01580a826977d78e19878b
goga <goga.gabelaia2@gmail.com>
2026-06-23 20:29:28 +0400

    feat: add Terraform infrastructure for Snowflake

---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "snowflake_user" {
+  description = "Snowflake username"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Snowflake password"
+  type        = string
+  sensitive   = true
+}
